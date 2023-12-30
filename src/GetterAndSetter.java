3016e2ad12e83dd03bf3edd4a419234882aba321
pravinsingh789 <pravinvns789@gmail.com>
2023-12-31 00:01:12 +0530

    day2

---
+++ src/GetterAndSetter.java
@@ -0,0 +1,27 @@
+package src;
+class Something{
+    private int a;
+    public void setA(int a){
+        this.a=a;
+    }
+    public int getA(){
+        return a;
+    }
+    private int b;
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
+public class GetterAndSetter {
+    public static void main(String[] args) {
+        Something sObj = new Something();
+        sObj.setA(10);
+        System.out.println(sObj.getA());
+        System.out.println(sObj.getB());
+
+    }
+}
